d6ba360c2cd820b5fb226cdebe25457d907aea6c
RBA-Kalikot <rbakalikotmusic@gmail.com>
2026-04-03 18:13:23 +0545

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('rba-v1').then((cache) => cache.addAll(['/', 'index.html', 'logo.png'])));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
