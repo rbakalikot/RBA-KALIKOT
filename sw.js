f6b1d79644614ff15a11aa8fa3ca5a62674f71d0
RBA-Kalikot <rbakalikotmusic@gmail.com>
2026-04-04 13:01:52 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,40 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('rba-v1').then((cache) => cache.addAll(['/', 'index.html', 'logo.png', 'logo.jpg'])));
+const cacheName = 'rba-v4';
+const assets = [
+  '/',
+  'index.html',
+  'logo.png',
+  'logo.jpg',
+  'bg.jpg',
+  'story.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('Caching assets');
+      cache.addAll(assets).catch(err => console.log('Cache error:', err));
+    })
+  );
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+
+// Active Service Worker
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Logic
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
